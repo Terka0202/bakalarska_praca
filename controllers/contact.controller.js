955a0c8cb292877deaeed2051f189a98b1dd69f3
Terezia Martiakova <martiakovat@gmail.com>
2025-03-04 20:32:49 +0100

    Odosielanie kontaktného formuláru na mail

--- controllers/contact.controller.js
+++ controllers/contact.controller.js
@@ -11,43 +11,32 @@ const getContactForm_ziak = (req, res) => {
 
 const postSendMail = async (req, res) => {
     const { name, surname, email, text } = req.body;
-    // len skusam
+
     try {
-        // Nastavenie SMTP servera (napr. MS Outlook)
         const transporter = nodemailer.createTransport({
-            host: "smtp.office365.com",
-            port: 587,
-            secure: false,
+            service: "gmail",
+            secure: true,
             auth: {
                 user: process.env.EMAIL_USER,
-                pass: process.env.EMAIL_PASS
-            }
+                pass: process.env.EMAIL_PASS,
+            },
         });
 
-        transporter.verify((error, success) => {
-            if (error) {
-                console.error("SMTP spojenie zlyhalo:", error);
-            } else {
-                console.log("SMTP spojenie úspešné, pripravené na odosielanie emailov");
-            }
-        });
-
-        const mailOptions = {
+        await transporter.sendMail({
             from: process.env.EMAIL_USER,
-            to: "martiakova22@gmail.com",
+            replyTo: email,
+            to: process.env.EMAIL_USER,
             subject: `Nová správa od ${name} ${surname}`,
             text: text,
-            replyTo: email
-        };
-        
-        await transporter.sendMail(mailOptions);
-        res.status(200).json({ success: "Správa bola odoslaná!" });
-        
+        });
+
+        res.redirect("/ziak/kontakt")
+    
     } catch (error) {
-        console.error(error);
+        console.error("Chyba pri odosielaní e-mailu:", error);
         return res.status(500).render("shared/500");
     }
-}
+};
 
 module.exports = {
     getContactForm,
